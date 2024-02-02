a23883b02e066dfaf726b4715a33c170011f1072
TranLoc2k3 <tranloc120603@gmail.com>
2024-02-02 22:18:24 +0700

    update buttons in message thread

--- app/components/main-content/MessageMainContent/header/HeaderBtns.tsx
+++ app/components/main-content/MessageMainContent/header/HeaderBtns.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import Image from "next/image";
 import ButtonCollapse from "./ButtonCollapse";
-import callVideoIcon from "@/public/icon/call_video.svg";
-import searchIcon from "@/public/icon/search.svg";
-import addFriendIcon from "@/public/icon/add_friend.svg";
-
+import { Video, Search, UserRoundPlus } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 function HeaderBtns() {
   return (
-    <div className="flex space-x-4">
-      <Image className="hover:bg-gray-200 rounded-lg" src={addFriendIcon} alt="search icon" height={20} width={20} />
-      <Image className="hover:bg-gray-200 rounded-lg" src={searchIcon} alt="search icon" height={20} width={20}/>
-      <Image className="hover:bg-gray-200 rounded-lg" src={callVideoIcon} alt="call video icon" height={24} width={24}/>
+    <div className="flex space-x-1">
+      <Button size="icon" variant="icon">
+        <UserRoundPlus width={20} height={20} strokeWidth={1.6} />
+      </Button>
+      <Button size="icon" variant="icon">
+        <Search width={20} height={20} strokeWidth={1.6} />
+      </Button>
+      <Button size="icon" variant="icon">
+        <Video width={20} height={20} strokeWidth={1.6} />
+      </Button>
       <ButtonCollapse />
     </div>
   );
