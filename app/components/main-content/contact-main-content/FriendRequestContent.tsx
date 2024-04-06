fecf386c35afc7540f2e57a96b50d265651f5fda
Ngọc Thắng <ngocthang.devweb@gmail.com>
2024-04-06 18:02:49 +0700

    Update Profile

--- app/components/main-content/contact-main-content/FriendRequestContent.tsx
+++ app/components/main-content/contact-main-content/FriendRequestContent.tsx
@@ -11,13 +11,13 @@ function FriendRequestContent() {
   useEffect(() => {
     const getAllFriendRequests = async () => {
       const res = await userAPI.getAllFriendRequests(
-        `/user/get-all-friend-requests/${session.data?.token?.user}`
+        `/user/get-all-friend-requests/${userPhone}`
       );
       setFriendRequest(res);
     };
 
     getAllFriendRequests();
-  }, [countFriendRequest, session.data?.token?.user]);
+  }, [countFriendRequest, userPhone]);
   return (
     <div>
       <h2>{`Lời mời đã nhận (${countFriendRequest})`}</h2>
