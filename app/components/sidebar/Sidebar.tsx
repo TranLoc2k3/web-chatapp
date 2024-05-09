8900ceff8488f458d08e3aaf38c07cdb303059ce
Ngọc Thắng <ngocthang.devweb@gmail.com>
2024-05-09 11:17:13 +0700

    thang

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,13 +1,16 @@
+"use client";
 import { cn } from "@/lib/utils";
 import ContentTab from "./content-tab/ContentTab";
 import MainTab from "./main-tab/MainTab";
+import { usePathname } from "next/navigation";
 
 function Sidebar() {
+  const pathName = usePathname();
   return (
     <div className={cn("flex bg-white flex-1", "md:flex-none")}>
       {/* Left Content */}
       <MainTab />
-      <ContentTab />
+      {pathName !== "/dashboard/map" && <ContentTab />}
     </div>
   );
 }
